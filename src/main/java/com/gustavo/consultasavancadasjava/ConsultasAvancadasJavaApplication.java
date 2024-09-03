6698d99a38e679d056e77d759d9f3cdfc8ae9e0e
Gustavo Guilherme Campelo Antunes <guguiantunes@hotmail.com>
2024-09-03 09:19:48 -0400

    first commit

---
+++ src/main/java/com/gustavo/consultasavancadasjava/ConsultasAvancadasJavaApplication.java
@@ -0,0 +1,13 @@
+package com.gustavo.consultasavancadasjava;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ConsultasAvancadasJavaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ConsultasAvancadasJavaApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/gustavo/consultasavancadasjava/ConsultasAvancadasJavaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.gustavo.consultasavancadasjava;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ConsultasAvancadasJavaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
